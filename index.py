0f8da7c1673aadf496a3c1d7272f88a656f524aa
JyotiDhiman <jyoti.dhiman@joshtechnologygroup.com>
2018-06-12 15:49:08 +0530

    Finish up first ML rampup

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+# Following: https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
+
 import pandas
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
@@ -15,11 +17,11 @@ from sklearn.svm import SVC
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = pandas.read_csv(url, names=names)
-print(dataset)
-print(dataset.shape)
-print(dataset.head(20))
-print(dataset.describe())
-print(dataset.groupby('class').size())
+# print(dataset)
+# print(dataset.shape)
+# print(dataset.head(20))
+# print(dataset.describe())
+# print(dataset.groupby('class').size())
 dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
 plt.show()
 # histograms
@@ -27,3 +29,67 @@ dataset.hist()
 plt.show()
 scatter_matrix(dataset)
 plt.show()
+
+# create dataset
+array = dataset.values
+X = array[:, 0:4]
+Y = array[:, 4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(
+    X, Y, test_size=validation_size, random_state=seed
+)
+
+# print(X_train)
+# print(X_validation)
+# print(Y_train)
+# print(Y_validation)
+
+# Test options and evaluation metric
+seed = 7
+scoring = 'accuracy'
+
+# Lets evaluate 6 different algorithms:
+#
+# Logistic Regression (LR)
+# Linear Discriminant Analysis (LDA)
+# K-Nearest Neighbors (KNN).
+# Classification and Regression Trees (CART).
+# Gaussian Naive Bayes (NB).
+# Support Vector Machines (SVM).
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = model_selection.KFold(n_splits=10, random_state=seed)
+    cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+# Compare Algorithms
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
+
+
+# Make predictions on validation dataset
+knn = KNeighborsClassifier()
+knn.fit(X_train, Y_train)
+predictions = knn.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
